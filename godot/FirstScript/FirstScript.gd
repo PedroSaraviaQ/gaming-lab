664b03068c8c2501e30fee232ef22e223b9fa29c
Pedro <pedro.saravia.q@uni.pe>
2023-12-29 22:15:43 -0500

    add signals

--- godot/FirstScript/FirstScript.gd
+++ godot/FirstScript/FirstScript.gd
@@ -12,21 +12,29 @@ var angular_speed = PI
 #* "_process" is called every frame (60 times per second by default).
 #* It takes a "delta" parameter, which is the time elapsed since the last frame.
 func _process(delta):
-	var direction = 0
+	# var direction = 0
 
 	#* Use the "Input" singleton (global object) to check for user input.
 	#! But you can also define the "_unhandled_input" function to handle it.
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
+	# if Input.is_action_pressed("ui_left"):
+	# 	direction = -1
+	# if Input.is_action_pressed("ui_right"):
+	# 	direction = 1
 
 	#* "rotation" is a property inherited from Node2D.
-	rotation += angular_speed * direction * delta
-	
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
+	# rotation += angular_speed * direction * delta
+	rotation += angular_speed * delta
+
+	# var velocity = Vector2.ZERO
+	# if Input.is_action_pressed("ui_up"):
+	# 	velocity = Vector2.UP.rotated(rotation) * speed
+
+	var velocity = Vector2.UP.rotated(rotation) * speed
 
 	#! The "position" property is of type "Vector2".
 	position += velocity * delta
+
+#* This is a signal, it will be emitted when the button is clicked.
+#? For this to work, we have commented out some lines of code.
+func _on_button_pressed():
+	set_process(not is_processing())
